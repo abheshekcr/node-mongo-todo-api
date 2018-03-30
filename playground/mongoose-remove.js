ec29c144eadfbe758dfbe3a11f690b60da773af1
abheshekcr <cabheshek@gmail.com>
2018-03-30 11:48:47 +0530

    Delete /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,7 @@
+const {mongoose} =require('./../server/db/mongoose');
+const {Todo}=require('./../server/models/todos');
+const {User}=require('./../server/models/users');
+
+//Todo.remove()
+//Todo.findByIdAndRemove
+
